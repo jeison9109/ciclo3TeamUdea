f393bdfd17603f0417ab81caa95c0a97cfd5c93d
yeison serna <yeisonivanserna@gmail.com>
2022-08-17 21:27:08 -0500

    fix:se configura aplication propierties se crean clases

--- src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -1,5 +1,6 @@
 package com.UdeA.Ciclo3;
 
+import com.UdeA.Ciclo3.modelos.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -14,6 +15,12 @@ public class Ciclo3Application {
 	@GetMapping("/hello")
 	public  String hello (){
 		return "Bienvenidos cilo 3";
+	}
+
+	@GetMapping("/test")
+	public  String test (){
+		Empresa emp = new Empresa("churritos","av 20#2-32","762322","9002322-1");
+		return emp.getNombre();
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
